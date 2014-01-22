cb5ca58fe1a4ffb50efa21925b3f6a679a5e729d
Ryan Hileman <lunixbochs@gmail.com>
2014-01-22 03:45:31 -0800

    add memory buffer for gdb

--- src/arch/msp430.py
+++ src/arch/msp430.py
@@ -33,11 +33,39 @@ class MSP430(object):
         return self.read_regs()[self.reg_names().index(name)]
 
 
+class MemBuffer(object):
+    def __init__(self, read, size=256):
+        self.read_raw = read
+        self.clear()
+        self.size = size
+
+    def clear(self):
+        self.pages = {}
+
+    def get_pages(self, start, end):
+        out = []
+        for base in xrange(start, end, self.size):
+            if base in self.pages:
+                page = self.pages[base]
+            else:
+                page = self.pages[base] = self.read_raw(base, self.size)
+            out.append(page)
+        return ''.join(out)
+
+    def read(self, addr, length):
+        start = addr & ~(self.size - 1)
+        end = addr + length
+        clip = addr - start
+        pages = self.get_pages(start, end)
+        return pages[clip:clip+length]
+
+
 class CorruptionMSP(object):
     def __init__(self, api):
         # super(self.__class__, self).__init__()
         self.api = api
         self.cpu = api.cpu
+        self.cache = MemBuffer(self.read_mem_raw)
 
     def read_regs(self):
         snapshot = self.cpu.snapshot()
@@ -47,19 +75,24 @@ class CorruptionMSP(object):
         for name, value in zip(self.reg_names(), regs):
             self.cpu.let(name, value)
 
-    def read_mem(self, addr, length):
+    def read_mem_raw(self, addr, length):
         return ''.join(
             self.cpu.read(i, min(length, 1024))
             for i in xrange(addr, addr + length, 1024)
         )[:length]
 
+    def read_mem(self, addr, length):
+        return self.cache.read(addr, length)
+
     def write_mem(self, addr, data):
         print 'write_mem stub'
 
     def _continue(self):
+        self.cache.clear()
         self.cpu._continue()
 
     def step(self, n=1):
+        self.cache.clear()
         self.cpu.step(n)
 
     def wait(self, timeout=30):
